35a9451ba2d1f372a59e031dd253399984b6d87c
filipmarchidan <marchidanfilip@yahoo.com>
2018-12-04 15:48:18 +0100

    a suge pula partea a 2 a

--- js/board.js
+++ js/board.js
@@ -2,25 +2,43 @@ function createBoard()
 {
     var tbl = document.getElementById("battleboard");
     var id = 0;
-    var horizontalIndicator = nextChar('a');
-    var verticalIndicator = -1;
+    var horizontalIndicator = 'A';
+    var verticalIndicator = 0;
+  
     for (var r=0; r < 11; r++) {
     var row = document.createElement("tr");
     for (var c=0; c < 11; c++) { 
         var cell = document.createElement("td");
-        cell.id = id++;
+       
         row.appendChild(cell);
-        cell.innerHTML = id;
-        if(c == 0)
+        if (c== 0 && r == 0)
+        {
+            cell.innerHTML = "MAP";
+            cell.style.backgroundColor = "#000000"
+            cell.style.color = "#ffffff"
+        }
+        if(c == 0 && r != 0)
         {
             cell.id = verticalIndicator++;
             cell.innerHTML = verticalIndicator;
+            cell.style.backgroundColor= "#800000"
+            cell.style.color = "#ffffff"
+            id--;
         }
-        if(r == 0)
+        if(r == 0  && c!=0)
         {
-            cell.id = nextChar('a');
+            cell.style.backgroundColor= "#800000"
+            cell.style.color = "#ffffff"
+            cell.id = horizontalIndicator;
             cell.innerHTML = horizontalIndicator;
+            horizontalIndicator = nextChar(horizontalIndicator);
         }
+        else
+        {
+            cell.id = id++;
+            cell.style.backgroundImage = "../photos/texture.png";
+        }
+       
      }
      tbl.appendChild(row);
     }
